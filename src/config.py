59fbd18f1f434c3b56b127dc34ef59b216aa763c
fabianHeflo <fabian.heflo@kartverket.no>
2025-06-19 10:10:07 +0200

    opprettet egen modul for config av augmentation

--- src/config.py
+++ src/config.py
@@ -5,25 +5,3 @@ ENDING_POINT = [255000.0000, 6799000.0000]
 # Base URL for fetching satellite images
 BASE_IMAGE_URL = "https://wms.geonorge.no/skwms1/wms.nib"
 GEOJSON_PATH = "turning_spaces.geojson"
-
-# Augmentation settings
-basic_aug = {
-    "flip_p": 0.5,
-    "rot90_p": 0.5,
-    "brightness_p": 0.3,
-}
-
-strong_aug = {
-    "flip_p": 0.7,
-    "rot90_p": 0.8,
-    "brightness_p": 0.5,
-    "shift": 0.1,
-    "scale": 0.3,
-    "rotate": 25,
-    "ssr_p": 0.8
-}
-
-augmentation_profiles = {
-    "basic": basic_aug,
-    "strong": strong_aug,
-}
