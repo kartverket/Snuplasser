5c9231c3b2995db570fae723659c9e72a3647d7b
fabianHeflo <fabian.heflo@kartverket.no>
2025-06-24 13:18:22 +0200

    fjerna normalisering og la til litt beskrivende tekst

--- src/dataProcessing/transform.py
+++ src/dataProcessing/transform.py
@@ -2,6 +2,15 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 
+"""
+Transformasjoner for trenings- og valideringsdata.
+Valg:
+- Ingen normalisering benyttes. Dette er bevisst for å unngå dataleakage og sikre at modellen fungerer på nye bilder uten behov for global statistikk.
+- Augmentering gjøres kun under trening for å gjøre modellen robust mot variasjoner i solforhold, skyggemønster og bildeorientering.
+- `ToTensorV2()` konverterer NumPy-arrays til PyTorch-tensorer, som forventet av modellen.
+"""
+
+
 def get_train_transforms(cfg):
     return A.Compose(
         [
@@ -10,11 +19,10 @@ def get_train_transforms(cfg):
             A.RandomBrightnessContrast(
                 p=cfg["brightness_p"]
             ),  # Endringer i solforhold, årstid, skygge eller skydetthet
-            A.Normalize(),
             ToTensorV2(),
         ]
     )
 
 
 def get_val_transforms():
-    return A.Compose([A.Normalize(), ToTensorV2()])
+    return A.Compose([ToTensorV2()])
