da34b36cbe97ca7537f3edf75ff9547d197521a9
EneTop <enes.topcu@kartverket.no>
2025-06-27 13:11:50 +0200

    Format Python files with black

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ BASE_IMAGE_URL = "https://wms.geonorge.no/skwms1/wms.nib"
 BASE_DOM_URL = "https://wms.geonorge.no/skwms1/wms.hoyde"
 GEOJSON_PATH = "/Volumes/land_topografisk-gdb_dev/external_dev/static_data/DL_SNUPLASSER/turning_spaces.geojson"
 
-SECRET_TOKEN = "" # settes inn når man kjører 
+SECRET_TOKEN = ""  # settes inn når man kjører
 
 TEST_BBOX = [331800, 6753200, 332500, 6754200]
 IMAGE_SIZE = [512, 512]
