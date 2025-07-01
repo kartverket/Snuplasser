9427d8f468f2ffc2ff5d8bf92ad82020d0ed188f
fabianHeflo <fabianHeflo>
2025-07-01 11:15:20 +0000

    satte opp en logger som skal være enkel å bruke i main

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+from pytorch_lightning.loggers import MLFlowLogger
+
+def get_logger(model_name: str, config: dict) -> MLFlowLogger:
+    logging_config = config.get("logging", {})
+    experiment_name = logging_config.get("experiment_name", "default_experiment")
+    tracking_uri = logging_config.get("tracking_uri", "file:./mlruns")
+
+    return MLFlowLogger(
+        experiment_name=experiment_name,
+        tracking_uri=tracking_uri,
+        tags={"model": model_name}
+    )
